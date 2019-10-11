9e3c07e0f1cb2239b3c4ea4fa6600d3f44355f8b
Liam McChesney <liam.mcey@gmail.com>
2019-10-11 10:24:13 -0600

    Finished Written Code

--- game.rb
+++ game.rb
@@ -9,6 +9,10 @@ class Tutorial < Gosu::Window
 
         @player = Player.new
         @player.warp(320, 240)
+
+        @star_anim = Gosu::Image.load_tiles("star.png", 25, 25)
+        @stars = Array.new
+        @font = Gosu::Font.new(20)
     end
 
     def update
@@ -22,15 +26,22 @@ class Tutorial < Gosu::Window
             @player.accelerate
         end
         @player.move
+        @player.collect_stars(@stars)
+
+        if rand(100) < 4 and @stars.size < 25
+            @stars.push(Star.new(@star_anim))
+        end
     end
 
     def draw
+        @background_image.draw(0, 0, ZOrder::BACKGROUND)
         @player.draw
-        @background_image.draw(0, 0, 0)
+        @stars.each { |star| star.draw }
+        @font.draw("Score: #{@player.score}", 10, 10, ZOrder::UI, 1.0, 1.0, Gosu::Color::YELLOW)
     end
 
     def button_down(id)
-        if id == Gosu::KB _ESCAPE
+        if id == Gosu::KB_ESCAPE
             close
         else
             super
@@ -41,6 +52,7 @@ end
 class Player
     def initialize
         @image = Gosu::Image.new("starfighter.bmp")
+        @beep = Gosu::Sample.new("beep.wav")
         @x = @y = @vel_x = @vel_y = @angle = 0.0
         @score = 0
     end
@@ -75,5 +87,45 @@ class Player
     def draw
         @image.draw_rot(@x, @y, 1, @angle)
     end
+
+    def score
+        @score
+    end
+
+    def collect_stars(stars)
+        stars.reject!  do |star|
+            if Gosu.distance(@x, @y, star.x, star.y) < 35
+                @score += 10
+                @beep.play
+                true
+            else
+                false
+            end
+        end
+    end
 end
+
+module ZOrder
+    BACKGROUND, STARS, UI = *0..3
+end
+
+class Star
+    attr_reader :x, :y
+
+    def initialize(animation)
+        @animation = animation
+        @color = Gosu::Color::BLACK.dup
+        @color.red = rand(256 - 40) + 40
+        @color.green = rand(256 - 40) + 40
+        @color.blue = rand(256 - 40) + 40
+        @x = rand * 640
+        @y = rand * 480
+    end
+
+    def draw
+        img = @animation[Gosu.milliseconds / 100 % @animation.size]
+        img.draw(@x - img.width / 2.0, @y - img.height / 2.0, ZOrder::STARS, 1, 1, @color, :add)
+    end
+end
+
 Tutorial.new.show
